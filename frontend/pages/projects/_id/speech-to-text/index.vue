daa28770f1eba30e4fcfef98c951d864754e7ba7
acidburnburn <shelvinskyi@pm.me>
2020-08-29 23:02:12 +0100

    finnaly speech player

---
+++ frontend/pages/projects/_id/speech-to-text/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <seq2seq-container />
+</template>
+
+<script>
+import Seq2seqContainer from '~/components/containers/annotation/Speech2textContainer'
+
+export default {
+  layout: 'annotation',
+
+  components: {
+    Seq2seqContainer
+  },
+
+  validate({ params, query }) {
+    return /^\d+$/.test(params.id) && /^\d+$/.test(query.page)
+  }
+}
+</script>
